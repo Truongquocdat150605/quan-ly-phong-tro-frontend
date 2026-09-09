5d7c4e57c6ec773bda9a77ef26bbb2c6afd933ba
TruongQuocDat <hungma668@@gmail.com>
2026-09-09 17:09:53 +0700

    fix(eslint): remove UTF-8 BOM from invoicePdfExport.js

--- src/utils/invoicePdfExport.js
+++ src/utils/invoicePdfExport.js
@@ -1,4 +1,4 @@
-﻿import jsPDF from "jspdf";
+import jsPDF from "jspdf";
 import autoTable from "jspdf-autotable";
 import { toast } from "react-toastify";
 import { addCustomFont } from "./pdfFont";
